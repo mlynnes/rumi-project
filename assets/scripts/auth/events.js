7bbefc6b6a4f1011ef79cd10ecdf903df55acc2c
mlynnes <micaelalsassano@gmail.com>
2017-05-05 10:32:42 -0400

    Events sign out
    Added onSignOut event to the events.js file.

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -28,11 +28,21 @@ const onChangePassword = function (event) {
     .catch(ui.changePasswordFailure)
 }
 
+const onSignOut = function (event) {
+  const data = getFormFields(this)
+  event.preventDefault()
+  api.signOut(data)
+  .then(ui.signOutSuccess)
+  .catch(ui.signOutFailure)
+}
+
 const addHandlers = () => {
   $('#sign-up').on('submit', onSignUp)
   $('#sign-in').on('submit', onSignIn)
   $('#change-password').hide()
   $('#change-password').on('submit', onChangePassword)
+  $('#sign-out').hide()
+  $('#sign-out').on('submit', onSignOut)
 }
 
 module.exports = {
