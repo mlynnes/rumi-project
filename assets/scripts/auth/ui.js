498c750cd3ea5f8ec92af3623971dcade3b190dc
mlynnes <micaelalsassano@gmail.com>
2017-05-05 10:17:23 -0400

    Ui sign in
    Added conditions for signInSuccess and signInFailure

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -3,14 +3,30 @@
 const store = require('../store.js')
 
 const signUpSuccess = (data) => {
-  console.log('sign in was successful')
+  console.log('sign up was successful')
+  $('#sign-up').hide()
 }
 
 const signUpFailure = (error) => {
   console.error(error)
 }
 
+const signInSuccess = (response) => {
+  store.user = response.user
+  console.log('sign in was successful')
+  $('#change-password').show()
+  $('#sign-out').show()
+  $('#sign-up').hide()
+  $('#sign-in').hide()
+}
+
+const signInFailure = (data) => {
+  console.log('error signing out')
+}
+
 module.exports = {
   signUpSuccess,
-  signUpFailure
+  signUpFailure,
+  signInSuccess,
+  signInFailure
 }
